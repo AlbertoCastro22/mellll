05446fccf173bb5d83ab4bd43e7fbbdd07869f0d
Usersone <Usersone@User>
2016-09-08 20:59:41 -0500

    primera parte

---
+++ Tarea2/src/tarea2/Pila.java
@@ -0,0 +1,48 @@
+
+package tarea2;
+
+/**
+ *
+ * @author Usersone
+ */
+public class Pila {
+    String[] pila;
+    Integer tope,tam;
+    public Pila(Integer tamanio){
+        tope=-1;
+        pila=new String[tamanio];
+        tam=tamanio;
+    }
+    public void push(String dato){
+        if(tope<tam-1){
+            tope++;
+            pila[tope]=dato;
+        }
+        else{
+            System.out.println("Error de desbordamiento...");
+        }
+    }
+    public String pop(){
+    String resultado =" ";
+    if(tope>-1){
+        resultado=pila[tope];
+        pila[tope]=null;
+        tope--;
+    }
+    else{
+        System.out.println("Error de subdesbordamiento...");
+    }
+    return resultado;
+    }
+    public String peek(){
+    String resultado="";
+    if(tope>-1){
+        resultado=pila[tope];
+    }
+    else{
+        System.out.println("Error de subdesbordamiento...");
+    }
+    return resultado;
+    }
+    
+}
